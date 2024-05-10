89ac9b68074a3983982e62bc8bf770469d55867f
nieyinliang <nieyinliang@wanjiedata.com>
2024-05-10 11:22:24 +0800

    switch golang version

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 RootPath=$(cd $(dirname $0) ; pwd)
-CfsBase="cubefs/cbfs-base:1.0-golang-1.17.13"
+CfsBase="cubefs/cbfs-base:1.0-golang-1.18.10"
 
 docker build -t ${CfsBase} -f ${RootPath}/Dockerfile ${RootPath}
